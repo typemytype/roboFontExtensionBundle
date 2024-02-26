4ca7a538c36c5b26c8bab86dd3d1a3b1e8338627
Roberto Arista <arista.rob@gmail.com>
2024-02-26 10:48:23 +0100

    improve package metadata

--- playground.py
+++ playground.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from Lib.extensionBundle import ExtensionBundle
+from Lib.robofontExtensionBundle import ExtensionBundle
 
 if __name__ == '__main__':
     bundle = ExtensionBundle.load(bundlePath=Path("tests/single_window_controller_template.roboFontExt"))
