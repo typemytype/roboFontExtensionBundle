e5d60e20fa76f0cc2a9cb2f67cc9b9e3e4bf9aee
Roberto Arista <arista.rob@gmail.com>
2024-02-27 12:37:31 +0100

    obsolete

--- playground.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-from Lib.robofontExtensionBundle import ExtensionBundle
-
-if __name__ == '__main__':
-    bundle = ExtensionBundle.load(bundlePath=Path("tests/single_window_controller_template.roboFontExt"))
-    bundle.unpack(destFolder=Path("unpack"))
